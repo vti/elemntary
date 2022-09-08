4e125b5d71a6c8623dbf23d5c8e14ba6eb32e08e
vti <vti@uptosmth.com>
2022-09-08 07:53:15 +0200

    Configurable backend

--- src/ui/main.js
+++ src/ui/main.js
@@ -4,9 +4,19 @@ import "./main.css";
 
 const app = createApp(App);
 
-//const MockApi = require("./api/mock.js");
-//app.provide("backend", new MockApi({ delay: 2000 }));
-const ElectronApi = require("./api/electron.js");
-app.provide("backend", new ElectronApi());
+let backend;
+if (process.env.VUE_APP_BACKEND == "Electron") {
+  const ElectronApi = require("./api/electron.js");
+
+  backend = new ElectronApi();
+} else if (process.env.VUE_APP_BACKEND == "Mock") {
+  const MockApi = require("./api/mock.js");
+
+  backend = new MockApi({ delay: 2000 });
+} else {
+  console.log(`Unknown backend: ${process.env.VUE_APP_BACKEND}`);
+}
+
+app.provide("backend", backend);
 
 app.mount("#app");
