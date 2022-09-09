b83de6b4298d209a2c16eae0f0bf03fa95d16050
vti <vti@uptosmth.com>
2022-09-09 21:20:40 +0200

    Install ipc handlers only once

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -2,44 +2,44 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   selectFile: () => ipcRenderer.invoke("dialog:openFile"),
-  onFileSelected: (callback) => ipcRenderer.on("file-selected", callback),
+  onFileSelected: (callback) => ipcRenderer.once("file-selected", callback),
   uploadMap: (deviceId, path) =>
     ipcRenderer.invoke("uploadMap", deviceId, path),
-  onMapUploaded: (callback) => ipcRenderer.on("map-uploaded", callback),
+  onMapUploaded: (callback) => ipcRenderer.once("map-uploaded", callback),
 
   listDevices: () => ipcRenderer.invoke("listDevices"),
-  onDeviceList: (callback) => ipcRenderer.on("device-list", callback),
+  onDeviceList: (callback) => ipcRenderer.once("device-list", callback),
 
   getFeatures: (deviceId) => ipcRenderer.invoke("getFeatures", deviceId),
-  onFeatureList: (callback) => ipcRenderer.on("feature-list", callback),
+  onFeatureList: (callback) => ipcRenderer.once("feature-list", callback),
   saveFeatures: (deviceId, features) =>
     ipcRenderer.invoke("saveFeatures", deviceId, features),
-  onFeaturesSaved: (callback) => ipcRenderer.on("features-saved", callback),
+  onFeaturesSaved: (callback) => ipcRenderer.once("features-saved", callback),
 
   takeScreenshot: (deviceId) => ipcRenderer.invoke("takeScreenshot", deviceId),
-  onScreenshot: (callback) => ipcRenderer.on("screenshot", callback),
+  onScreenshot: (callback) => ipcRenderer.once("screenshot", callback),
 
   getApkInfo: (deviceId) => ipcRenderer.invoke("getApkInfo", deviceId),
-  onApkInfo: (callback) => ipcRenderer.on("apk-info", callback),
+  onApkInfo: (callback) => ipcRenderer.once("apk-info", callback),
 
   clearCache: (deviceId) => ipcRenderer.invoke("clearCache", deviceId),
-  onCacheCleared: (callback) => ipcRenderer.on("cache-cleared", callback),
+  onCacheCleared: (callback) => ipcRenderer.once("cache-cleared", callback),
 
   restartApplication: (deviceId) =>
     ipcRenderer.invoke("restartApplication", deviceId),
   onApplicationRestarted: (callback) =>
-    ipcRenderer.on("application-restarted", callback),
+    ipcRenderer.once("application-restarted", callback),
 
   reboot: (deviceId) => ipcRenderer.invoke("reboot", deviceId),
-  onRebooted: (callback) => ipcRenderer.on("rebooted", callback),
+  onRebooted: (callback) => ipcRenderer.once("rebooted", callback),
 
   getWebServerInfo: (deviceId) =>
     ipcRenderer.invoke("getWebServerInfo", deviceId),
-  onWebServerInfo: (callback) => ipcRenderer.on("web-server-info", callback),
+  onWebServerInfo: (callback) => ipcRenderer.once("web-server-info", callback),
   startWebServer: (deviceId) => ipcRenderer.invoke("startWebServer", deviceId),
   onWebServerStarted: (callback) =>
-    ipcRenderer.on("web-server-started", callback),
+    ipcRenderer.once("web-server-started", callback),
   stopWebServer: (deviceId) => ipcRenderer.invoke("stopWebServer", deviceId),
   onWebServerStopped: (callback) =>
-    ipcRenderer.on("web-server-stopped", callback),
+    ipcRenderer.once("web-server-stopped", callback),
 });
